2a8d1168da53537f41ae62f00d45eda5e25d290c
lukasz <slaskww@gmail.com>
2021-01-13 13:37:00 +0100

    Add Test class for Quicksort alg

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -34,7 +34,7 @@ public class SortTest {
     @Test
     public void testThreeRowArray(){
 
-        int[] arr = new int[]{7,3, 8};
+        int[] arr = new int[]{7, 3, 8};
         int[] sortedArr = Quicksort.sortArray( arr, 0, arr.length - 1);
 
         assertArrayEquals( sortedArr, new int[]{3,7, 8} );
@@ -47,6 +47,16 @@ public class SortTest {
         int[] sortedArr = Quicksort.sortArray( arr, 0, arr.length - 1);
 
         assertArrayEquals( sortedArr, new int[]{1, 2, 3, 4, 5, 6, 7} );
+    }
+
+
+    @Test
+    public void testTwentyRowArray(){
+
+        int[] arr = new int[]{1, 19, 2 ,18, 3, 17, 4 ,5 ,16, 15 ,10, 6 ,8 ,9 ,7 , 14 ,13 ,12 ,11, 0};
+        int[] sortedArr = Quicksort.sortArray( arr, 0, arr.length - 1);
+
+        assertArrayEquals( sortedArr, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19} );
     }
 
     @Test
