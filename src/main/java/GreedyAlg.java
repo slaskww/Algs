bc210c61c0e87db49e4138f58f057c5532df63d6
lukasz <slaskww@gmail.com>
2021-01-27 15:22:58 +0100

    Add GreedyAlg and GreedyAlgTest classes

---
+++ src/main/java/GreedyAlg.java
@@ -0,0 +1,11 @@
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+public class GreedyAlg {
+
+    public static Map<String, Set<String>> fillMapOfUSStatesWithColor(Map<String, Set<String>> uSStates){
+
+        return Collections.emptyMap();
+    }
+}
