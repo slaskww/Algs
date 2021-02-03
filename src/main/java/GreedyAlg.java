d37e0e54c5a386b79ab7b54cd93ceb7b21f5be93
lukasz <slaskww@gmail.com>
2021-02-03 15:55:07 +0100

    Refactor fillMapOfUSStatesWithColor method

--- src/main/java/GreedyAlg.java
+++ src/main/java/GreedyAlg.java
@@ -8,73 +8,75 @@ import java.util.stream.Collectors;
 @Slf4j
 public class GreedyAlg {
 
-    public static Map<String, Set<String>> fillMapOfUSStatesWithColor(Map<String, USState> uSStates){
+    public static Map<String, Set<String>> fillMapOfUSStatesWithColor(Map<String, USState> uSStates) {
 
-       if(uSStates.isEmpty()) throw new IllegalArgumentException();
-
-
-       Stack<String> colors = new Stack<String>(){{addAll( Arrays.asList("red", "gray", "blue", "green", "purple", "orange", "yellow", "black", "white") );}};
-       Set<String> processedStates = new HashSet<>();
-       Map<String, Set<String>> mapOfColouredStates = new HashMap<>();
-       Iterator<String> statesToProcess = uSStates.keySet().iterator();
-
-       log.info( String.valueOf( uSStates.size()  ));
-
-       while(statesToProcess.hasNext()){
-         String processedState =  statesToProcess.next();
-         log.info( "Processed State=" + processedState );
-         Set<String> neighboursOfProcessedState = uSStates.get( processedState ).getNeighboursStates();
-           boolean isStateColoredWithExistingColor = false;
-
-            if(mapOfColouredStates.isEmpty()){
-                mapOfColouredStates.put( colors.pop(), new HashSet<String>(){{add( processedState );}} );
-            } else{
-
-             Iterator<Map.Entry<String, Set<String>>> mapOfColouredStatesIterator =  mapOfColouredStates.entrySet().iterator();
+        if (uSStates.isEmpty()) throw new IllegalArgumentException();
 
 
-             while(mapOfColouredStatesIterator.hasNext() && !isStateColoredWithExistingColor){
-                 Map.Entry<String, Set<String>> mapOfColouredStatesEntry = mapOfColouredStatesIterator.next();
+        Stack<String> colors = new Stack<String>() {
+            {
+                addAll( Arrays.asList( "red", "gray", "blue", "green", "purple", "orange", "yellow", "black", "white" ) );
+            }
+        };
 
-                 log.info( "In  while(mapOfColouredStatesIterator.hasNext() && !isStateColoredWithExistingColor)" );
+        Map<String, Set<String>> mapOfColouredStates = new HashMap<>();
+        Iterator<String> statesToProcess = uSStates.keySet().iterator();
 
-                 if(!checkIfStatesForCertainColorContainsNeighbours(mapOfColouredStatesEntry.getValue(), neighboursOfProcessedState)){
-                     Set<String> actualSetOfStatesForColor = mapOfColouredStatesEntry.getValue();
-                     actualSetOfStatesForColor.add( processedState );
+        while (statesToProcess.hasNext()) {
+            String processedState = statesToProcess.next();
+            Set<String> neighboursOfProcessedState = uSStates.get( processedState ).getNeighboursStates();
+            boolean isUsedNewColorToFillAState = true;
 
-                     mapOfColouredStates.replace(mapOfColouredStatesEntry.getKey(), actualSetOfStatesForColor  );
-                     isStateColoredWithExistingColor = true;
-                     log.info(String.valueOf( mapOfColouredStates.get( "white" ).size() ));
-                 }
-             }
+            if (mapOfColouredStates.isEmpty()) {
+                mapOfColouredStates.put( colors.pop(), new HashSet<String>() {{
+                    add( processedState );
+                }} );
+            } else {
 
-             if(!isStateColoredWithExistingColor){
-                 mapOfColouredStates.put( colors.pop(),  new HashSet<String>(){{add( processedState );}} );
-             }
+                Iterator<Map.Entry<String, Set<String>>> mapOfColouredStatesIterator = mapOfColouredStates.entrySet().iterator();
+
+
+                while (mapOfColouredStatesIterator.hasNext() && isUsedNewColorToFillAState) {
+                    Map.Entry<String, Set<String>> mapOfColouredStatesEntry = mapOfColouredStatesIterator.next();
+
+                    if (!checkIfStatesForCertainColorContainsNeighbours( mapOfColouredStatesEntry.getValue(), neighboursOfProcessedState )) {
+                        Set<String> actualSetOfStatesForColor = mapOfColouredStatesEntry.getValue();
+                        actualSetOfStatesForColor.add( processedState );
+
+                        mapOfColouredStates.replace( mapOfColouredStatesEntry.getKey(), actualSetOfStatesForColor );
+                        isUsedNewColorToFillAState = false;
+                    }
+                }
+
+                if (isUsedNewColorToFillAState) {
+                    mapOfColouredStates.put( colors.pop(), new HashSet<String>() {{
+                        add( processedState );
+                    }} );
+                }
             }
 
-       }
+        }
 
-       mapOfColouredStates.forEach( (s, strings) -> log.info( s +"{" + strings.stream().collect( Collectors.joining(", ") ) + "}" ) );
+        mapOfColouredStates.forEach( (s, strings) -> log.info( s + "{" + strings.stream().collect( Collectors.joining( ", " ) ) + "}" ) );
         return mapOfColouredStates;
 
     }
 
     @Data
     @AllArgsConstructor
-    public static class USState{
+    public static class USState {
 
         private String name;
         private Set<String> neighboursStates;
     }
 
-    private static boolean checkIfStatesForCertainColorContainsNeighbours(Set<String> statesForColor, Set<String> neighbours){
+    private static boolean checkIfStatesForCertainColorContainsNeighbours(Set<String> statesForColor, Set<String> neighbours) {
 
         Iterator<String> neighboursIter = neighbours.iterator();
-        while(neighboursIter.hasNext()){
+        while (neighboursIter.hasNext()) {
             String neighbour = neighboursIter.next();
-            if(statesForColor.contains( neighbour )) return true;
+            if (statesForColor.contains( neighbour )) return true;
         }
-    return false;
+        return false;
     }
 }
