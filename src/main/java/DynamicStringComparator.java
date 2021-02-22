12cb13ec9af320c3d498481205dff373d7465f22
lukasz <slaskww@gmail.com>
2021-02-22 14:23:43 +0100

    Refactor findOptimalContents method

--- src/main/java/DynamicStringComparator.java
+++ src/main/java/DynamicStringComparator.java
@@ -1,51 +1,64 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 
+@Slf4j
 public class DynamicStringComparator implements DynamicAlg<String> {
 
 
     int[][] comparativeMatrix;
     private final List<String> wordListToCompare;
-    private final String chosenWord;
+    private final String originalWord;
 
-    public DynamicStringComparator(List<String> wordListToCompare, String chosenWord) {
+    public DynamicStringComparator(List<String> wordsToCompare, String originalWord) {
 
-        this.chosenWord = chosenWord;
-        this.wordListToCompare = wordListToCompare;
+        this.originalWord = originalWord;
+        this.wordListToCompare = wordsToCompare;
     }
 
     @Override
     public Set<String> findOptimalContents() {
 
-        if(chosenWord.equals( "" )) throw new IllegalArgumentException("Chosen word must not be empty");
+        if(originalWord.equals( "" )) throw new IllegalArgumentException("Chosen word must not be empty");
         if(wordListToCompare.isEmpty()) throw new IllegalArgumentException("List of similar words must not be empty");
 
 
         Set<String> bestMatchedWords = new HashSet<>();
+        StringBuilder bestMatchedSequenceAsString = new StringBuilder();
+
         int bestMatchingValue = 0;
 
         for(String wordToCompare: wordListToCompare){
 
             if(!wordToCompare.equals( "" )){
 
-                int rows = chosenWord.length();
+                int rows = originalWord.length();
                 int cols = wordToCompare.length();
 
                 comparativeMatrix = new int[rows][cols];
+
+                bestMatchedSequenceAsString.append( "\n\n" );
 
                 for(int i = 0; i < rows; i++){
 
 
                     for(int j = 0; j < cols; j++){
 
-                        char rowChar = chosenWord.charAt( i );
+                        char rowChar = originalWord.charAt( i );
                         char columnChar = wordToCompare.charAt( j );
                         int currentTableElementValue = 0;
 
-                        if(rowChar == columnChar) currentTableElementValue = 1 + getElementValue(i -1, j-1);
-                        else currentTableElementValue = maxValue(getElementValue(i, j-1), getElementValue(i -1, j));
+                        if(rowChar == columnChar) {
+                            currentTableElementValue = 1 + getElementValue(i -1, j-1);
+                            bestMatchedSequenceAsString.append( "[" ).append( columnChar ).append( "]" );
+                        }
+                        else {
+                            currentTableElementValue = maxValue(getElementValue(i, j-1), getElementValue(i -1, j));
+
+                        }
 
                         comparativeMatrix[i][j] = currentTableElementValue;
                     }
@@ -57,8 +70,10 @@ public class DynamicStringComparator implements DynamicAlg<String> {
 
             }
         }
+        log.info("Best matched sequence: " +  bestMatchedSequenceAsString.toString() );
         return bestMatchedWords;
     }
+
 
     private int getElementValue(int row, int column){
 
